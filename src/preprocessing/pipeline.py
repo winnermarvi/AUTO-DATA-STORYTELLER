b8b3f023ef377aadd9c3dd084c170c544cf59eac
winnermarvi <winnermarvi3@gmail.com>
2026-05-04 15:32:41 +0530

    Created scaling function in preprocessing part,  not completed yet

--- src/preprocessing/pipeline.py
+++ src/preprocessing/pipeline.py
@@ -2,6 +2,7 @@ import pandas as pd
 from .missing_handler import missing_handler
 from .outlier_handler import handle_outliers
 from .encoding import encode_data
+from .scaler import scaling
 
 df = pd.read_csv("data/sample1.csv")
 print("raw data:")
@@ -14,6 +15,8 @@ def preprocess_data(df):
     df = handle_outliers(df)
 
     df = encode_data(df)
+
+    df = scaling(df)
 
     return df
 
---
+++ src/preprocessing/scaler.py
@@ -0,0 +1,18 @@
+
+#scaling huge value for model to understand properly
+def scaling(df):
+
+    numeric_cols = df.select_dtypes(include = ['float64','int64']).columns.to_list()
+
+    for i in numeric_cols:
+
+        if df[i].nunique() < 2 and sorted(df[i].unique()) == ['0','1']:
+            continue
+
+        mean = df[i].mean()
+        std = df[i].std()
+
+        if std != 0:
+            df[i] = (df[i] - mean) / std
+
+    return df
