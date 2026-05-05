b0a41f1a7b9b9bf0c186e9e86bf82a82a75ce467
winnermarvi <winnermarvi3@gmail.com>
2026-05-05 23:50:08 +0530

    Generate insights done till numeric summary will continue with this file tommorow

--- src/insights/insights_generator.py
+++ src/insights/insights_generator.py
@@ -3,12 +3,14 @@
 def generate_insights(report):
 
     insights = []
-
+    
+    #Rows and columns
     rows = report['shape']['rows']
     cols = report['shape']['columns']
 
     insights.append(f"Dataset contains {rows} rows & {cols} columns")
 
+    #missing values 
     total_missing_value = 0
     max_missing = 0
     max_missing_val_col = ""
@@ -25,15 +27,24 @@ def generate_insights(report):
         insights.append(f"Total missing values are {total_missing_value}")
         insights.append(f"Column {max_missing_val_col} has the highest missing values ({max_missing} values)")
 
+    #numeric columns
     for key in report['numerical_summary']:
         
         insights.append(f"Average {key} is {report['numerical_summary'][key]['mean']:.2f}")
-        insights.append(f"{key} ranges from {report['numerical_summary'][key]['min']} to {report['numerical_summary'][key]['max']}")
+        insights.append(f"{key} ranges from {int(report['numerical_summary'][key]['min'])} to {int(report['numerical_summary'][key]['max'])}")
         
-        
-        if float(report['numerical_summary'][key]['std'])/float(report['numerical_summary'][key]['mean']) > 0.3 and report['numerical_summary'][key]['mean'] != 0:
-            insights.append(f"{key} shows high variation")
+        if report['numerical_summary'][key]['mean'] == 0:
+            if float(report['numerical_summary'][key]['std']) > 0.3:
+                insights.append(f"{key} shows high variation")
+            else:
+                insights.append(f"{key} shows low variation")
         else:
-            insights.append(f"{key} shows less variation")
+            if float(report['numerical_summary'][key]['std'])/abs(float(report['numerical_summary'][key]['mean'])) > 0.3:
+                insights.append(f"{key} shows high variation")
+            else:
+                insights.append(f"{key} shows low variation")
 
+    #categoric columns
+
+    
     return insights
