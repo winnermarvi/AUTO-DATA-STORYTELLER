ea8b3f0ce6e46413b8dfd4eb4b9f47dcb3e3283d
winnermarvi <winnermarvi3@gmail.com>
2026-06-12 21:43:13 +0530

    Started with regression folder

--- src/ml/regression/train_models.py
+++ src/ml/regression/train_models.py
@@ -1,7 +1,12 @@
-
+from sklearn.linear_model import LinearRegression
 
 def train_linear_regression(X_train,y_train):
-    pass
+    
+    model = LinearRegression()
+
+    model.fit(X_train, y_train)
+
+    return model
 
 def train_decision_tree(X_train,y_train):
     pass
